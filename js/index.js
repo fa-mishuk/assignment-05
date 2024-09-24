afd727c97d51c95deabce7b006d462063026765a
Mishuk <fatemaa.mishuk@gmail.com>
2024-09-25 01:41:52 +0600

    received my new addition

--- js/index.js
+++ js/index.js
@@ -12,16 +12,18 @@ document.getElementById("history-btn").addEventListener('click', function () {
     showColorByIdd("history-btn")
     showColorByIdd("donate-btn")
     showColorByIdd("donate-card")
+
+    document.getElementById("history-container").classList.remove('hidden')
 })
 
 
-document.getElementById("logo-btn").addEventListener('click', function(e) {
+document.getElementById("logo-btn").addEventListener('click', function (e) {
     e.preventDefault()
     window.location.href = 'home.html'
 })
 
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navBanner = document.getElementById('nav-banner');
     if (window.scrollY > 100) {
         navBanner.classList.add('blurred');
@@ -41,6 +43,8 @@ document.getElementById('donate1').addEventListener('click', function (event) {
     const donateInput1 = getInputValueById("donate-input1");
     const modal = document.getElementById('my_modal_2');
     const closeBtn = document.getElementById('close-btn');
+    const donateP1 = document.getElementById('donate-p1').innerText
+
 
     if (!isNaN(donateInput1) && Number(donateInput1) > 0) {
 
@@ -59,6 +63,18 @@ document.getElementById('donate1').addEventListener('click', function (event) {
             closeBtn.addEventListener('click', function () {
                 modal.close();
             });
+
+            // ----------------
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="grid gap-4 p-8 border mx-7 md:mx-24 lg:mx-24 rounded-xl mb-5">
+                <p class="font-bold lg:text-xl"><span>${donateInput1} Taka is </span>${donateP1}</p>
+                <p class="text-[#111111B3] text-sm lg:text-[16px]">Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh
+                Standard Time)</p>
+            </div>`;
+           
+            document.getElementById("history-container").appendChild(div)
+            // ----------------
         }
         else {
             alert('Invalid donate amount')
@@ -81,6 +97,7 @@ document.getElementById('donate2').addEventListener('click', function (event) {
     const donateInput2 = getInputValueById("donate-input2");
     const modal = document.getElementById('my_modal_2');
     const closeBtn = document.getElementById('close-btn');
+    const donateP2 = document.getElementById('donate-p2').innerText
 
     if (!isNaN(donateInput2) && Number(donateInput2) > 0) {
 
@@ -98,6 +115,18 @@ document.getElementById('donate2').addEventListener('click', function (event) {
             modal.showModal();
             closeBtn.addEventListener('click', function () {
                 modal.close();
+
+                // ----------------
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="grid gap-4 p-8 border mx-7 md:mx-24 lg:mx-24 rounded-xl mb-5">
+                <p class="font-bold lg:text-xl"><span>${donateInput2} Taka is </span>${donateP2}</p>
+                <p class="text-[#111111B3] text-sm lg:text-[16px]">Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh
+                Standard Time)</p>
+            </div>`;
+           
+            document.getElementById("history-container").appendChild(div)
+            // ----------------
             });
         }
         else {
@@ -121,6 +150,7 @@ document.getElementById('donate3').addEventListener('click', function (event) {
     const donateInput3 = getInputValueById("donate-input3");
     const modal = document.getElementById('my_modal_2');
     const closeBtn = document.getElementById('close-btn');
+    const donateP3 = document.getElementById('donate-p3').innerText
 
     if (!isNaN(donateInput3) && Number(donateInput3) > 0) {
 
@@ -129,7 +159,7 @@ document.getElementById('donate3').addEventListener('click', function (event) {
         const donatedBalance3 = balance3 + donateInput3;
 
         const reservedAmount = getDonateBalance("reserved-amount")
-       
+
 
         if (donateInput3 <= reservedAmount) {
             document.getElementById('balance3').innerText = donatedBalance3;
@@ -138,6 +168,18 @@ document.getElementById('donate3').addEventListener('click', function (event) {
             modal.showModal();
             closeBtn.addEventListener('click', function () {
                 modal.close();
+
+                // ----------------
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="grid gap-4 p-8 border mx-7 md:mx-24 lg:mx-24 rounded-xl mb-5">
+                <p class="font-bold lg:text-xl"><span>${donateInput3} Taka is </span>${donateP3}</p>
+                <p class="text-[#111111B3] text-sm lg:text-[16px]">Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh
+                Standard Time)</p>
+            </div>`;
+           
+            document.getElementById("history-container").appendChild(div)
+            // ----------------
             });
         }
         else {
